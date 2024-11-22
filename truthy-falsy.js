8cd631b74372bc43171288677e7419fa2dc874ec
jDARKTV <160490674+jDARKTV@users.noreply.github.com>
2024-11-22 12:23:02 -0300

    carga_js-tr-falsy
    carga_js-tr-falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,34 @@
+const x= 0;
+
+//boalean
+const usúarioLogado = true
+const contapaga= false
+
+//truthy ou falsy
+// 0 -> false
+//1-> true
+
+ console.log/ (0 == false)// retorna true
+
+ console.log(""==false)// retorna true
+ console.log("null e undefinided")
+
+ //undefinided
+ //null-> vazio ou nada
+
+ let minhaVar;
+ let varNull;
+
+ console.log(minhaVar)
+ console.log(varNull)
+
+ console.log("quando o undefinided está em sua forma pura")
+
+ let número=3;
+ let test= "alura";
+
+ console.log(typeof número);
+ console.log(typeof texto);
+
+ console.log(typeof MinhaVar);
+ console.log(typeof varNull);
